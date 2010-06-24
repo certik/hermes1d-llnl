65e034bd25d6c534b21285c7232a68a559cf29cd
Ondrej Certik <ondrej@certik.cz>
2010-06-24 11:58:15 -0700

    Add Dirac solver

---
+++ examples/dirac/main.cpp
@@ -0,0 +1,140 @@
+#include "hermes1d.h"
+
+#include "python_api.h"
+
+#include "_hermes1d_api.h"
+
+int N_elem = 40;                         // number of elements
+static int N_eq = 2;
+double A = 0, B = 50;                     // domain end points
+int P_init = 5;                           // initial polynomal degree
+
+/* We solve A*x = E*B*x, where A, B are matrices composed of 2x2 blocks */
+
+double kappa = 1;
+
+// speed of light (in atomic units)
+double c = 137.036;
+
+double A_00(int num, double *x, double *weights,
+                double *u, double *dudx, double *v, double *dvdx,
+                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                void *user_data)
+{
+    double val = 0;
+    for(int i = 0; i<num; i++) {
+        double r = x[i];
+        // specify V:
+        double V = -1/r;
+        //double V = 0;
+        val += u[i]*V*v[i]*weights[i];
+    }
+    return val;
+}
+
+double A_11(int num, double *x, double *weights,
+                double *u, double *dudx, double *v, double *dvdx,
+                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                void *user_data)
+{
+    double val = 0;
+    for(int i = 0; i<num; i++) {
+        double r = x[i];
+        // specify V:
+        double V = -1/r;
+        //double V = 0;
+        val += u[i]*(V-2*c*c)*v[i]*weights[i];
+    }
+    return val;
+}
+
+double A_01(int num, double *x, double *weights,
+                double *u, double *dudx, double *v, double *dvdx,
+                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                void *user_data)
+{
+    double val = 0;
+    for(int i = 0; i<num; i++) {
+        double r = x[i];
+        val += c*(-u[i]*dvdx[i] + u[i]*(kappa/r)*v[i])*weights[i];
+    }
+    return val;
+}
+
+double A_10(int num, double *x, double *weights,
+                double *u, double *dudx, double *v, double *dvdx,
+                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                void *user_data)
+{
+    double val = 0;
+    for(int i = 0; i<num; i++) {
+        double r = x[i];
+        val += c*(u[i]*dvdx[i] + u[i]*(kappa/r)*v[i])*weights[i];
+    }
+    return val;
+}
+
+double B_00(int num, double *x, double *weights,
+                double *u, double *dudx, double *v, double *dvdx,
+                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
+                void *user_data)
+{
+    double val = 0;
+    for(int i = 0; i < num; i++) {
+        double r = x[i];
+        val += u[i]*v[i]*weights[i];
+    }
+    return val;
+}
+
+/******************************************************************************/
+int main(int argc, char* argv[]) {
+  // create mesh
+  Mesh *mesh = new Mesh(A, B, N_elem, P_init, N_eq);
+  mesh->set_bc_left_dirichlet(0, 0);
+  mesh->set_bc_right_dirichlet(0, 0);
+
+  // variable for the total number of DOF
+  int N_dof = mesh->assign_dofs();
+  printf("ndofs: %d\n", N_dof);
+
+  // register weak forms
+  DiscreteProblem *dp1 = new DiscreteProblem();
+  dp1->add_matrix_form(0, 0, A_00);
+  dp1->add_matrix_form(0, 1, A_01);
+  dp1->add_matrix_form(1, 0, A_10);
+  dp1->add_matrix_form(1, 1, A_11);
+  DiscreteProblem *dp2 = new DiscreteProblem();
+  dp2->add_matrix_form(0, 0, B_00);
+  dp2->add_matrix_form(1, 1, B_00);
+
+  CooMatrix *mat1 = new CooMatrix(N_dof);
+  CooMatrix *mat2 = new CooMatrix(N_dof);
+
+  dp1->assemble_matrix(mesh, mat1);
+  dp2->assemble_matrix(mesh, mat2);
+
+  Python p;
+
+  p.exec("print 'Python initialized'");
+  p.push("A", c2py_CooMatrix(mat1));
+  p.push("B", c2py_CooMatrix(mat2));
+  p.exec("from utils import solve_eig_numpy, solve_eig_pysparse");
+  p.exec("eigs = solve_eig_numpy(A.to_scipy_coo(), B.to_scipy_coo())");
+  //p.exec("eigs = solve_eig_pysparse(A.to_scipy_coo(), B.to_scipy_coo())");
+  p.exec("from utils import show_eigs");
+  p.exec("show_eigs(eigs)");
+
+  if (import_hermes1d___hermes1d())
+      throw std::runtime_error("");
+  p.push("mesh",  c2py_Mesh(mesh));
+  p.exec("from plot import plot_eigs, plot_file");
+  p.exec("plot_eigs(mesh, eigs)");
+  printf("Done.\n");
+  return 0;
+}
