3a64f9bc26297c0a71c187228617bef710f47eda
Ondrej Certik <ondrej@certik.cz>
2010-07-06 13:49:02 -0700

    Remove the residual

--- examples/schroedinger/main.cpp
+++ examples/schroedinger/main.cpp
@@ -48,24 +48,6 @@ double rhs(int num, double *x, double *weights,
 
 double E;
 
-double residual(int num, double *x, double *weights,
-                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                double *v, double *dvdx, void *user_data)
-{
-    double *u = &u_prev[0][0][0];
-    double *dudx = &du_prevdx[0][0][0];
-    double val = 0;
-    for(int i = 0; i<num; i++) {
-        double coeff;
-        coeff = 0.5*x[i]*x[i]*dudx[i]*dvdx[i] -u[i]*v[i]*x[i]
-            + 0.5 * (l + 1)*l *u[i]*v[i];
-        coeff -= E*u[i]*v[i]*x[i]*x[i];
-        val += coeff*weights[i];
-    }
-    return val;
-}
-
 /******************************************************************************/
 int main(int argc, char* argv[]) {
   // create mesh
@@ -83,13 +65,8 @@ int main(int argc, char* argv[]) {
   DiscreteProblem *dp2 = new DiscreteProblem();
   dp2->add_matrix_form(0, 0, rhs);
 
-  DiscreteProblem *dp3 = new DiscreteProblem();
-  dp3->add_vector_form(0, residual);
-
-  // allocate Jacobi matrix and residual
   CooMatrix *mat1 = new CooMatrix(N_dof);
   CooMatrix *mat2 = new CooMatrix(N_dof);
-  double *y_prev = new double[N_dof];
 
   dp1->assemble_matrix(mesh, mat1);
   dp2->assemble_matrix(mesh, mat2);
@@ -103,18 +80,7 @@ int main(int argc, char* argv[]) {
   p.exec("eigs = solve_eig_numpy(A.to_scipy_coo(), B.to_scipy_coo())");
   //p.exec("eigs = solve_eig_pysparse(A.to_scipy_coo(), B.to_scipy_coo())");
   p.exec("E, v = eigs[0]");
-  int n;
 
-  double *res = new double[N_dof];
-  E = py2c_double(p.pull("E"));
-  printf("E=%.10f\n", E);
-  E = -0.5;
-  dp3->assemble_vector(mesh, res);
-  // calculate L2 norm of residual vector
-  double res_norm = 0;
-  for(int i=0; i<N_dof; i++) res_norm += res[i]*res[i];
-  res_norm = sqrt(res_norm);
-  printf("L2 norm of the residual: %f\n", res_norm);
 
 
   Linearizer l(mesh);
