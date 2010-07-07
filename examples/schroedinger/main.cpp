b137a09663911e56d73b903d53cc3cb0e218631d
Ondrej Certik <ondrej@certik.cz>
2010-07-07 02:42:33 -0700

    Split main.cpp into forms.h/cpp

--- examples/schroedinger/main.cpp
+++ examples/schroedinger/main.cpp
@@ -1,52 +1,14 @@
 #include "hermes1d.h"
 
 #include "python_api.h"
-
 #include "_hermes1d_api.h"
+
+#include "forms.h"
 
 static int N_eq = 1;
 int N_elem = 40;                         // number of elements
 double A = 0, B = 20;                     // domain end points
 int P_init = 2;                           // initial polynomal degree
-
-double l = 0;
-
-double lhs(int num, double *x, double *weights,
-                double *u, double *dudx, double *v, double *dvdx,
-                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                void *user_data)
-{
-    double val = 0;
-    for(int i = 0; i<num; i++) {
-        double coeff;
-        double r = x[i];
-        // specify r^2*V:
-        // Hydrogen:
-        double rrV = -r; // r^2 * (-1/r) = -r
-        // Harmonic oscillator:
-        //double rrV = r*r*r*r; // r^2 * (r^2) = r^4
-        coeff = 0.5*r*r*dudx[i]*dvdx[i] + (rrV + 0.5 * (l + 1)*l) *u[i]*v[i];
-        val += coeff*weights[i];
-    }
-    return val;
-}
-
-double rhs(int num, double *x, double *weights,
-                double *u, double *dudx, double *v, double *dvdx,
-                double u_prev[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                double du_prevdx[MAX_SLN_NUM][MAX_EQN_NUM][MAX_QUAD_PTS_NUM],
-                void *user_data)
-{
-    double val = 0;
-    for(int i = 0; i < num; i++) {
-        double r = x[i];
-        val += u[i]*v[i]*r*r*weights[i];
-    }
-    return val;
-}
-
-double E;
 
 /******************************************************************************/
 int main(int argc, char* argv[]) {
