be116d944270577b4d6c973fd46be71ca13bbf4e
Ondrej Certik <ondrej@certik.cz>
2010-07-07 03:48:53 -0700

    schroedinger.py now runs

--- examples/schroedinger/schroedinger.py
+++ examples/schroedinger/schroedinger.py
@@ -2,21 +2,21 @@
 
 from hermes1d import Mesh, CooMatrix
 
-from _forms import assemble_schroedinger
+#from _forms import assemble_schroedinger
 from utils import solve_eig_numpy, solve_eig_pysparse
 from plot import plot_eigs, plot_file
 
 N_eq = 1
 N_elem = 40                         # number of elements
-A = 0; B = 20                     # domain end points
-P_init = 2                           # initial polynomal degree
+R = 20                              # right hand side of the domain
+P_init = 2                          # initial polynomal degree
 
 def main():
-    mesh = Mesh(A, B, N_elem, P_init, N_eq)
+    mesh = Mesh(0, R, N_elem, P_init, N_eq)
     N_dof = mesh.assign_dofs()
     A = CooMatrix(N_dof)
     B = CooMatrix(N_dof)
-    assemble_schroedinger(mesh, A, B)
+    #assemble_schroedinger(mesh, A, B)
     eigs = solve_eig_numpy(A.to_scipy_coo(), B.to_scipy_coo())
     #eigs = solve_eig_pysparse(A.to_scipy_coo(), B.to_scipy_coo())
     E, v = eigs[0]
