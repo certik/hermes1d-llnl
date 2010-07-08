06fa80cbf2f81b2b9448613e7dd76300af43e603
Ondrej Certik <ondrej@certik.cz>
2010-07-08 02:45:29 -0700

    Use fekete points for solutions

--- examples/schroedinger/schroedinger.py
+++ examples/schroedinger/schroedinger.py
@@ -3,26 +3,37 @@
 import sys
 sys.path.insert(0, "../..")
 
+from numpy import arange
+
 from hermes1d import Mesh
 from hermes1d.solvers.eigen import solve_eig_numpy, solve_eig_pysparse
+from hermes1d.h1d_wrapper.h1d_wrapper import FESolution
+from hermes1d.fekete.fekete import Function, Mesh1D
 from hermes_common._hermes_common import CooMatrix
 
 from _forms import assemble_schroedinger
 from plot import plot_eigs, plot_file
 
-N_elem = 40                         # number of elements
-R = 20                              # right hand side of the domain
-P_init = 2                          # initial polynomal degree
+
+N_elem = 4                         # number of elements
+R = 150                            # right hand side of the domain
+P_init = 6                         # initial polynomal degree
 
 def main():
-    mesh = Mesh(0, R, N_elem, P_init)
+    pts = arange(0, R, float(R)/(N_elem+1))
+    orders = [P_init]*N_elem
+    mesh = Mesh(pts, orders)
     N_dof = mesh.assign_dofs()
     A = CooMatrix(N_dof)
     B = CooMatrix(N_dof)
     assemble_schroedinger(mesh, A, B, l=0)
-    eigs = solve_eig_numpy(A.to_scipy_coo(), B.to_scipy_coo())
-    #eigs = solve_eig_pysparse(A.to_scipy_coo(), B.to_scipy_coo())
-    #E, v = eigs[0]
+    eigs = solve_eig_numpy(A.to_scipy_coo(), B.to_scipy_coo())[:4]
+    for E, eig in eigs:
+        print E
+        s = FESolution(mesh, eig)
+        f = s.to_discrete_function()
+        print "plotting"
+        f.plot(False)
     plot_eigs(mesh, eigs)
 
 if __name__ == "__main__":
