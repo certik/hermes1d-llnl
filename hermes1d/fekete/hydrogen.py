c18c91e2d42a1312d77b1a0800a085c3bd71fc5a
Ondrej Certik <ondrej@certik.cz>
2010-07-09 15:05:35 -0700

    Polish hydrogen

--- hermes1d/fekete/hydrogen.py
+++ hermes1d/fekete/hydrogen.py
@@ -16,8 +16,20 @@ def laguerre_poly(alpha, n, x):
     return r
 
 def R_nl(n, l, r, a):
+    """
+    Returns the radial Hydrogen wavefunction, as a function of "r".
+
+    "a" is the Bohr radius.
+    """
     return S(1)/n * sqrt(factorial(n-l-1)/factorial(n+l)/a**3) * (2*r/n/a)**l * \
             laguerre_poly(2*l+1, n-l-1, 2*r/n/a)*exp(-r/n/a)
+
+def R_nl_numeric(n, l, x):
+    """
+    Returns the exact floating point value of the R_nl at the (float) point
+    'x'.
+    """
+    return float(R_nl(n, l, x, 1).evalf())
 
 if __name__ == "__main__":
     var("alpha z")
@@ -26,6 +38,10 @@ if __name__ == "__main__":
     for n in range(1, 7):
         for l in range(0, n):
             print "(%d, %d): %s" % (n, l, R_nl(n, l, r, 1))
+
+    print
+    print R_nl(4, 0, S(1)/2, 1).evalf()
+    print R_nl_numeric(4, 0, 0.5)
     #from pylab import plot, show
     #from numpy import arange
     #x = arange(0, 20, 0.1)
