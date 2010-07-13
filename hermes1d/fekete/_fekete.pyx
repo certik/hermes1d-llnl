7f303420406de9d2295df2f76c9d36851bef2cea
Ondrej Certik <ondrej@certik.cz>
2010-07-13 02:29:21 -0700

    Turn off bounds checking

--- hermes1d/fekete/_fekete.pyx
+++ hermes1d/fekete/_fekete.pyx
@@ -1,3 +1,5 @@
+cimport cython
+
 from numpy cimport ndarray, double_t
 
 def eval_polynomial1(coeffs, x):
@@ -7,6 +9,7 @@ def eval_polynomial1(coeffs, x):
         r += a*x**(n-i-1)
     return r
 
+@cython.boundscheck(False)
 def eval_polynomial2(ndarray[double_t] coeffs not None, double x):
     cdef double r=0
     cdef unsigned n = len(coeffs)
