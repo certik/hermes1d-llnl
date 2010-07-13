c95b23c493439e2137bcd2a236fadf95113ac25e
Ondrej Certik <ondrej@certik.cz>
2010-07-13 02:23:34 -0700

    Add more types

--- hermes1d/fekete/_fekete.pyx
+++ hermes1d/fekete/_fekete.pyx
@@ -1,4 +1,4 @@
-from numpy cimport ndarray
+from numpy cimport ndarray, double_t
 
 def eval_polynomial1(coeffs, x):
     r = 0
@@ -7,10 +7,10 @@ def eval_polynomial1(coeffs, x):
         r += a*x**(n-i-1)
     return r
 
-def eval_polynomial2(ndarray coeffs, double x):
+def eval_polynomial2(ndarray[double_t] coeffs not None, double x):
     cdef double r=0
-    cdef int n = len(coeffs)
-    cdef int i
+    cdef unsigned n = len(coeffs)
+    cdef unsigned i
     for i in range(n):
         r += coeffs[i]*x**(n-i-1)
     return r
