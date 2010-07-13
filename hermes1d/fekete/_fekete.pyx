1224d3e103f120c9eb2ee7ad5f4f505c644122b1
Ondrej Certik <ondrej@certik.cz>
2010-07-13 02:13:27 -0700

    Faster version implemented by adding types

--- hermes1d/fekete/_fekete.pyx
+++ hermes1d/fekete/_fekete.pyx
@@ -1,6 +1,16 @@
+from numpy cimport ndarray
+
 def eval_polynomial1(coeffs, x):
     r = 0
     n = len(coeffs)
     for i, a in enumerate(coeffs):
         r += a*x**(n-i-1)
+    return r
+
+def eval_polynomial2(ndarray coeffs, double x):
+    cdef double r=0
+    cdef int n = len(coeffs)
+    cdef int i
+    for i in range(n):
+        r += coeffs[i]*x**(n-i-1)
     return r
