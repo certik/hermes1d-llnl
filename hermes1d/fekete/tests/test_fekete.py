89f11a70f85aa211a3660fa2ea3921100847e75e
Ondrej Certik <ondrej@certik.cz>
2010-07-26 17:15:48 -0700

    Tests for the h1_norm() added

--- hermes1d/fekete/tests/test_fekete.py
+++ hermes1d/fekete/tests/test_fekete.py
@@ -236,6 +236,7 @@ def test8():
 
 def test_l2():
     eps = 1e-12
+    eps_low = 1e-10
     func = lambda x: sin(x)
     mesh1 = Mesh1D((0, pi), (20,))
     mesh2 = Mesh1D((0, pi/2, pi), (20, 20))
@@ -251,11 +252,17 @@ def test_l2():
     assert abs(g.l2_norm(method="FE")-sqrt(pi/2)) < eps
     assert abs(h.l2_norm(method="FE")-sqrt(pi/4)) < eps
 
+    assert abs(f.h1_norm()-sqrt(pi)) < eps_low
+    assert abs(g.h1_norm()-sqrt(pi)) < eps_low
+    assert abs(h.h1_norm()-sqrt(pi/2)) < eps_low
+
     func = lambda x: cos(x)
     mesh1 = Mesh1D((0, pi/4, pi/2, 3*pi/4, pi), (20, 20, 20, 20))
     f = Function(func, mesh1)
     assert abs(f.l2_norm(method="Fekete")-sqrt(pi/2)) < eps
     assert abs(f.l2_norm(method="FE")-sqrt(pi/2)) < eps
+
+    assert abs(f.h1_norm()-sqrt(pi)) < eps
 
 def test_power():
     eps = 1e-12
