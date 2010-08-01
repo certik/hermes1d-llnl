b76cab1b881dd8a201dce4a3d3bb5ec46c83eb0e
Ondrej Certik <ondrej@certik.cz>
2010-08-01 12:51:39 -0700

    Plot both p-FEM and uniform p-FEM

--- examples/schroedinger/graphs/plot_silver.py
+++ examples/schroedinger/graphs/plot_silver.py
@@ -3,6 +3,7 @@
 from pylab import plot, show, savefig, grid, gca, legend, figure, title, \
         xlabel, ylabel
 
+import silver_uniformpfem
 import silver_pfem
 
 def do_plot(x, y, n, l, color="k"):
@@ -21,5 +22,6 @@ n_eig = 50
 l = 0
 for i in range(n_eig):
     n = l+1+i
-    do_plot(silver_pfem.R_x[l], silver_pfem.R_y[n, l], n, l)
+    do_plot(silver_uniformpfem.R_x[l], silver_uniformpfem.R_y[n, l], n, l)
+    do_plot(silver_pfem.R_x[l], silver_pfem.R_y[n, l], n, l, "b")
 savefig("conv_l_0.png")
