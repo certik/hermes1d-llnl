f7107c3decfaf331dd75ff1f27abf62484b04c01
Ondrej Certik <ondrej@certik.cz>
2010-08-03 21:48:50 -0700

    Better graph

--- examples/schroedinger/graphs/plot_silver.py
+++ examples/schroedinger/graphs/plot_silver.py
@@ -8,9 +8,12 @@ import silver_uniformpfem4
 import silver_pfem
 import silver_hpfem
 
-def do_plot(x, y, n, l, color="k"):
+def do_plot(x, y, n, l, color="k", label=""):
     n_r = n - l - 1
-    plot(x, y, color + "-", label="$R_{%d%d}$" % (n, l))
+    if n_r == 0:
+        plot(x, y, color + "-", label=label)
+    else:
+        plot(x, y, color + "-")
 
     grid(True)
     ax = gca()
@@ -18,7 +21,7 @@ def do_plot(x, y, n, l, color="k"):
     ylabel("$E_{num}-E$")
     ax.set_yscale("log")
     title("l=%d" % l)
-    #legend()
+    legend()
 
 n_eig = 50
 l = 0
@@ -26,9 +29,11 @@ print "Saving to conv_l_0.png"
 for i in range(n_eig):
     n = l+1+i
     do_plot(silver_uniformpfem50.R_x[l], silver_uniformpfem50.R_y[n, l],
-            n, l, "k")
+            n, l, "y", "uniform $p$-FEM, 50 elms")
     do_plot(silver_uniformpfem4.R_x[l], silver_uniformpfem4.R_y[n, l],
-            n, l, "b")
-    do_plot(silver_pfem.R_x[l], silver_pfem.R_y[n, l], n, l, "g")
-    do_plot(silver_hpfem.R_x[l], silver_hpfem.R_y[n, l], n, l, "y")
+            n, l, "k", "uniform $p$-FEM, 4 elms")
+    #do_plot(silver_pfem.R_x[l], silver_pfem.R_y[n, l],
+    #        n, l, "b", "$p$-FEM")
+    do_plot(silver_hpfem.R_x[l], silver_hpfem.R_y[n, l],
+            n, l, "r", "$hp$-FEM")
 savefig("conv_l_0.png")
